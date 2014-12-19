e803cf451792dd362b27f507982a441ce565f087
Kevin Murphy <km86463@gmail.com>
2014-12-19 11:36:25 -0700

    modified routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 'static_pages#index'
-  get 'about' => 'static_pages#about_me'
-  get 'contact' => 'static_pages#contact_me'
+  get 'about', to: 'static_pages#about_me'
+  get 'contact', to: 'static_pages#contact_me'
 
   resources :posts
 
